0c633afbd0f5115c322219b4ee281f110ac36269
Leozinho2831 <leoferraz1818@gmail.com>
2024-06-20 19:56:50 -0300

    commit: javascript para esconder e mostrar imagens

--- Assets/Javascript/buttonsMenu.js
+++ Assets/Javascript/buttonsMenu.js
@@ -14,42 +14,54 @@ const images = {
 };
 
 function hiddenImages(){
-    Object.values(images).forEach(imagesList => {
+    Object.values(images).forEach(imagesList =>{
         imagesList.forEach(imagesAdd => {
-            imagesAdd.classList.add('hiddenImages');
+            imagesAdd.style.cssText = 'display: none';
         });
     });
 };
 
-function showAllImages() {
-    Object.values(images).forEach(imagesList => {
-        imagesList.forEach(image => {
-            image.classList.remove('hiddenImages');
+function showAllImages(){
+    Object.values(images).forEach(imagesList =>{
+        imagesList.forEach(imagesRemove => {
+            imagesRemove.style.cssText = 'display: flex';
         });
     });
 }
 
 function showImages(imagesList){
     imagesList.forEach(imagesRemove =>{
-        imagesRemove.classList.remove('hiddenImages');
+        imagesRemove.style.cssText = 'display: flex';
     });
 };
+
+function resetButtonStyles() {
+    Object.values(buttons).forEach(button =>{
+        button.style.cssText = 'color: var(--greenItens-color); border: 1px solid #DBDFD0; background-color: transparent';
+    });
+}
 
 function selectMenu(){
 
     const clickButton = event.target;
 
     hiddenImages();
+    resetButtonStyles();
 
     if(clickButton === buttons.button1){
         showAllImages();
+        buttons.button1.style.cssText = 'background-color: var(--redItens-color); color: #FFFFFF; border: 0;';
     } else if(clickButton === buttons.button2){
         showImages(images.images1);
+        buttons.button2.style.cssText = 'background-color: var(--redItens-color); color: #FFFFFF; border: 0;';
     } else if(clickButton === buttons.button3){
-        showImages(images.images1);
+        showImages(images.images2);
+        buttons.button3.style.cssText = 'background-color: var(--redItens-color); color: #FFFFFF; border: 0;';
     } else if(clickButton === buttons.button4){
-        showImages(images.images1);
+        showImages(images.images3);
+        buttons.button4.style.cssText = 'background-color: var(--redItens-color); color: #FFFFFF; border: 0;';
     } else if(clickButton === buttons.button5){
         showImages(images.images4);
+        buttons.button5.style.cssText = 'background-color: var(--redItens-color); color: #FFFFFF; border: 0;';
     }
 }
